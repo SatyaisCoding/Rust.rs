050072a7a0799363b22e2d2d63f9952d8bb547e6
Satya Prakash <95564166+SatyaisCoding@users.noreply.github.com>
2024-07-29 16:37:58 +0530

    pattern-matching(enums++)

--- src/main.rs
+++ src/main.rs
@@ -483,6 +483,47 @@ fn main() {
         }
 
         // We will be implementing the calcuate_area function in the pattern matching section.
+
+
+
+
+
+
+
+    // ********** Pattern Matching *********** //
+
+    // Let you pattern match across various variants of an enum and run some logic.
+
+    // Define an enum called Shape
+enum Shape {
+    Circle(f64),  // Variant with associated data (radius)
+    Square(f64),  // Variant with associated data (side length)
+    Rectangle(f64, f64),  // Variant with associated data (width, height)
+}
+
+// Function to calculate area based on the shape
+fn calculate_area(shape: Shape) -> f64 {
+    match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,   // if u need singl;e stmt 
+        Shape::Square(side_length) => side_length * side_length,
+        Shape::Rectangle(width, height) =>{                ----->    // if u need multiple stmt
+             width * height
+        },
+    }
+}
+
+fn main() {
+    // Create instances of different shapes
+    let circle = Shape::Circle(5.0);
+    let square = Shape::Square(4.0);
+    let rectangle = Shape::Rectangle(3.0, 6.0);
+
+    // Calculate and print the areas
+    println!("Area of circle: {}", calculate_area(circle));
+    println!("Area of square: {}", calculate_area(square));
+    println!("Area of rectangle: {}", calculate_area(rectangle));
+}
+
     
     
 }
